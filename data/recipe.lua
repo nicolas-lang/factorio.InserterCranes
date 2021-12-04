fbc400144ae5ad99e42c354abbadcab96961e489
Nicolas Lang <git@nicolas-lang.de>
2021-12-04 23:59:33 +0100

    add K2 crushing recipe

--- data/recipe.lua
+++ data/recipe.lua
@@ -8,7 +8,7 @@ local function make_crane_recipe(recipeName, newName, wide)
 	local recipe = util.table.deepcopy(data.raw["recipe"][recipeName])
 	--log(serpent.block(recipe))
 	if recipe.icon or recipe.icons then
-		make_layered_icon(recipe,wide)
+		make_layered_icon(recipe, wide)
 	end
 	recipe.name = newName
 	recipe.energy_required = (recipe.energy_required or 1) * scale
@@ -22,6 +22,33 @@ local function make_crane_recipe(recipeName, newName, wide)
 	recipe.ingredients = ingredients
 	--log("recipe"..serpent.block(recipe))
 	data:extend({recipe})
+	-- add k2 crushing recipe
+	if mods["Krastorio2"] then
+		local crushing_recipe = {
+			type = "recipe",
+			name = newName .. "-to-parts",
+			icon = "__base__/graphics/icons/inserter.png",
+			icon_size = 64,
+			icon_mipmaps = 4,
+			category = "crushing",
+			subgroup = "inserter-cranes",
+			hide_from_player_crafting = true,
+			always_show_made_in = true,
+			allow_as_intermediate = false,
+			allow_intermediates = false,
+			always_show_products = true,
+			energy_required = 1,
+			enabled = true,
+			ingredients = {
+				{newName, 1}
+			},
+			results = {}
+		}
+		for _, v in pairs(recipe.ingredients) do
+			table.insert(crushing_recipe.results, {type = "item", name = v[1],  amount = v[2], probability = 0.75})
+		end
+		data:extend({crushing_recipe})
+	end
 end
 
 return make_crane_recipe
