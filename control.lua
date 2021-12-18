857938b5b50722ca67f4fe1a801e631eacc36c5e
Nicolas Lang <git@nicolas-lang.de>
2021-12-18 18:22:26 +0100

    cleaned up events

--- control.lua
+++ control.lua
@@ -1,1 +1,14 @@
-require("script.reload-tech-unlock")
+local reload_tech_unlock = require("script.reload-tech-unlock")
+-------------------------------------------------------------------------------
+script.on_configuration_changed(
+	function()
+		reload_tech_unlock.reload_tech_unlock()
+	end
+)
+-------------------------------------------------------------------------------
+script.on_event(
+	defines.events.on_force_created,
+	function()
+		reload_tech_unlock.reload_tech_unlock()
+	end
+)
--- script/reload-tech-unlock.lua
+++ script/reload-tech-unlock.lua
@@ -1,7 +1,8 @@
+local me = {}
 -------------------------------------------------------------------------------------
 --Enable Recipes if tech researched
 -------------------------------------------------------------------------------------
-local function reload_tech_unlock()
+function me.reload_tech_unlock()
 	--log("reload_tech_unlock")
 	for _, force in pairs(game.forces) do
 		for _, tech in pairs(force.technologies) do
@@ -14,21 +15,11 @@ local function reload_tech_unlock()
 				for _, effect in pairs(tech.effects) do
 					if effect.type == "unlock-recipe" then
 						force.recipes[effect.recipe].enabled = unlock_state
-						--log(effect.recipe .. " enabled: " .. tostring(unlock_state))
+					--log(effect.recipe .. " enabled: " .. tostring(unlock_state))
 					end
 				end
 			end
 		end
 	end
 end
--------------------------------------------------------------------------------------
-local function script_on_configuration_changed()
-	reload_tech_unlock()
-end
---
-local function event_on_force_created()
-	reload_tech_unlock()
-end
--------------------------------------------------------------------------------------
-script.on_configuration_changed(script_on_configuration_changed)
-script.on_event(defines.events.on_force_created, event_on_force_created)
+return me
