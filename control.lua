400fa98ecb3286006993f4068a2fd0dbf927797e
Nicolas Lang <git@nicolas-lang.de>
2021-12-18 18:34:17 +0100

    initial "bob's inserters" compatibility (just blacklisting for now)

--- control.lua
+++ control.lua
@@ -1,8 +1,36 @@
 local reload_tech_unlock = require("script.reload-tech-unlock")
 -------------------------------------------------------------------------------
+local function blacklist_inserters()
+	remote.call(
+		"bobinserters",
+		"blacklist_inserters",
+		{
+			"nco-wide-crane",
+			"nco-wide-filter-crane",
+			"nco-crane",
+			"nco-filter-crane"
+		}
+	)
+	if script.active_mods["Krastorio2"] then
+		remote.call(
+			"bobinserters",
+			"blacklist_inserters",
+			{
+				"nco-superior-wide-crane",
+				"nco-superior-wide-filter-crane",
+				"nco-superior-crane",
+				"nco-superior-filter-crane"
+			}
+		)
+	end
+end
+-------------------------------------------------------------------------------
 script.on_configuration_changed(
 	function()
 		reload_tech_unlock.reload_tech_unlock()
+		if script.active_mods["bobinserters"] then
+			blacklist_inserters()
+		end
 	end
 )
 -------------------------------------------------------------------------------
