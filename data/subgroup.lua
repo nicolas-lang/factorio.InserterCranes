b5c0107bbdd6af2bfd338fea61cbba3eab736862
Nicolas Lang <git@nicolas-lang.de>
2021-12-19 19:18:47 +0100

    fixed typo

--- data/subgroup.lua
+++ data/subgroup.lua
@@ -4,7 +4,7 @@ if not data.raw["item-subgroup"]["inserter-cranes"] then
 	local group = "logistics"
 	local order = "c"
 	if mods["boblogistics"] then
-		group = "bob-logistics",
+		group = "bob-logistics"
 		order = "c-z-a"
 	end
 	data:extend(
